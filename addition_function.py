edda00ee331628814d49b4e01cf8c5c4685edfdd
Den <deniss.boltunovs@gmail.com>
2024-06-27 20:32:41 +0300

    main method added

---
+++ addition_function.py
@@ -0,0 +1,8 @@
+
+
+def addition(*args):
+    """function sums all arguments and returns value"""
+    total = 0
+    for arg in args:
+        total += arg
+    return total
---
+++ main.py
@@ -0,0 +1,7 @@
+import addition_function
+
+result = addition_function.addition(5,6) # should return 11
+result1 = addition_function.addition(3,6,11) # should return 20
+
+print(result)
+print(result1)
