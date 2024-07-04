18b6a908a25abc2d82ca04e750b59cf2bcee9a76
Den <deniss.boltunovs@gmail.com>
2024-07-04 11:45:06 +0300

    adj

--- importing_data/test_pipeline.py
+++ importing_data/test_pipeline.py
@@ -1,11 +1,12 @@
 import pytest
 from pipeline_extract import extract
 
-@pytest.fixture
-def raw_data():
+# @pytest.fixture
+# def raw_data():
+#     data = extract("my_data.xlsx")
+#     return data
+
+
+def test_raw_data():
     data = extract("my_data.xlsx")
-    return data
-
-
-def test_raw_data(raw_data):
-    assert len(raw_data.columns) == 12
+    assert len(data.columns) == 12
